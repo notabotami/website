17640a041f3db98235459a6eb6ccaf5271529470
Julio Cervantes <notabotami@gmail.com>
2015-08-05 13:16:01 -0500

    few config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,8 @@ BastaScheduler::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
 
 end
