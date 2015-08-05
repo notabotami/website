4352ba0ab14debdeff9ed834417b082b35324dae
Julio Cervantes <notabotami@gmail.com>
2015-08-05 14:31:25 -0500

    mailer

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -11,7 +11,17 @@ end
 
   def contact_us_submit
 
-    redirect contact_us_path
+
+    @email=params['email']
+    @name=params['name']
+    @phone=params['phone']
+    @contact_option=params['contact_option']
+    @question=params['question']
+    @prefer_spanish=params['prefer_spanish']
+
+    ExampleMailer.sample_email(@email,@name,@phone,@contact,@question,@prefer_spanish).deliver!
+
+    redirect_to contact_us_path
   end
 
 end
--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,7 +1,15 @@
 class ExampleMailer < ActionMailer::Base
   default :from => "example@example.com"
 
-  def sample_email
+  def sample_email(email,name,phone,contact_option,question,prefer_spanish)
+    @email = email
+    @name = name
+    @phone = phone
+    @contact_option= contact_option
+    @question= question
+    @prefer_spanish=prefer_spanish
+
     mail(:to => "blurgiamtrash@gmail.com", :subject => "Sample Email")
   end
+
 end
