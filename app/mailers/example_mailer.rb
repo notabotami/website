01d404b1687c75b0344e00918106a9b4f2779821
Julio Cervantes <notabotami@gmail.com>
2015-08-06 18:11:42 -0500

    asdf

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -27,7 +27,7 @@ class ExampleMailer < ActionMailer::Base
 
     attachments["#{file.original_filename}"] = File.read(file.path)
     time = Time.new
-    mail(:to => "48streetincometax@gmail.com",
+    mail(:to => "blurgiamtrash@gmail.com",
          :subject => '48thStreet Employee Application - ' + @name + ' - ' + time.strftime("%Y-%m-%d %H:%M:%S %Z"))
   end
 
